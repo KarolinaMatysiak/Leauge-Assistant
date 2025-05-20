da4fd0fcf883ba5347b82a61ab193e1786d0e77c
karolincia <karolina.matysiak@microsoft.wsei.edu.pl>
2025-05-20 22:55:24 +0200

    add auth middleware for champ update

--- ChampionService/src/middlewares/auth.js
+++ ChampionService/src/middlewares/auth.js
@@ -1,11 +1,17 @@
-const isLoggedIn = (req,res,next) =>
- {
-    if (req.user) 
-    {
-        next();
+const jwt = require("jsonwebtoken");
+
+const authenticate = (req, res, next) => {
+  if (req.headers.authorization) {
+    try {
+        const token = req.headers.authorization.slice('Bearer '.length)
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      next()
+    } catch (err) {
+      res.status(401).send("Nie jestes zalogowany");
     }
-    else
-    {
-        res.status(401).send('Nie jestes zalogowany');
-    }
-}
+  } else {
+    res.status(401).send("Nie jestes zalogowany");
+  }
+};
+
+module.exports = { authenticate };
--- ChampionService/src/routes/championsRoute.js
+++ ChampionService/src/routes/championsRoute.js
@@ -1,9 +1,10 @@
 const championController = require('../controllers/championsController')
+const auth = require('../middlewares/auth')
 
 
 function registerRoutes(app)
 {
-  app.post('/champions-update', championController.updateChampions)  
+  app.post('/champions-update', auth.authenticate, championController.updateChampions)  
   app.get('/champions', championController.getChampions)  
 }
 
